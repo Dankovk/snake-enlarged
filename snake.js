d643cba9d12dce709fb25b2c7a194db3d246449b
Dankov Alex <alexander.dankov@geenee.me>
2024-12-11 21:50:44 +0200

    FIXES

--- snake.js
+++ snake.js
@@ -9,15 +9,15 @@ const EAST = {x: 1, y: 0}
 const WEST = {x: -1, y: 0}
 
 
-const snakeFromFriendSnake = state => {
-    return {
-        ...state,
-        moves: [...state.friendMoves],
-        snake: [...state.friendSnake],
-        isSuper: state.friendIsSuper,
-        inverted: false
-    }
-};
+// const snakeFromFriendSnake = state => {
+//     return {
+//         ...state,
+//         moves: [...state.friendMoves],
+//         snake: [...state.friendSnake],
+//         isSuper: state.friendIsSuper,
+//         inverted: false
+//     }
+// };
 
 // Point operations
 const pointEq = p1 => p2 => p1.x == p2.x && p1.y == p2.y
@@ -29,20 +29,20 @@ const willEatSuperApple = state => pointEq(nextHead(state))(state.superApple)
 // const willFriendEatSuperApple = state => willEatSuperApple(snakeFromFriendSnake(state))
 
 
-const willSnakeHeadsIntersect = state =>  {
-    pointEq(nextHead(state), nextFriendHead(state))
-    return false;
-}
+// const willSnakeHeadsIntersect = state =>  {
+//     pointEq(nextHead(state), nextFriendHead(state))
+//     return false;
+// }
 const willCrash = state => {
-    return state.snake.find(pointEq(nextHead(state))) || willSnakeHeadsIntersect(state)
+    return state.snake.find(pointEq(nextHead(state)))
 }
 // const friendWillCrash = state => willCrash(snakeFromFriendSnake(state))
 const validMove = move => state => state.moves[0].x + move.x != 0 || state.moves[0].y + move.y != 0
 
 // Next values based on state
 const nextMoves = state => state.moves.length > 1 ? dropFirst(state.moves) : state.moves
-const nextApple = state => willEat(state) || willFriendEat(state) ? rndPos(state) : state.apple
-const nextSuperApple = state => willEatSuperApple(state) || willFriendEatSuperApple(state) ? rndPos(state) : state.superApple
+const nextApple = state => willEat(state) ? rndPos(state) : state.apple
+const nextSuperApple = state => willEatSuperApple(state) ? rndPos(state) : state.superApple
 const nextSuper = (state) => {
     if (state.isSuper) {
         return !willEatSuperApple(state)
@@ -59,9 +59,9 @@ const nextHead = state => state.snake.length == 0
         y: mod(state.rows)(state.snake[0].y + state.moves[0].y)
     }
 
-const nextFriendHead = (state) => {
-    return nextHead(snakeFromFriendSnake(state));
-}
+// const nextFriendHead = (state) => {
+//     return nextHead(snakeFromFriendSnake(state));
+// }
 
 
 const nextSnake = state => willCrash(state) ? [] : (willEatAppleOrSuperApple(state) ? enlargeSnake(state) : [nextHead(state)].concat(dropLast(state.snake)));
@@ -107,8 +107,8 @@ const next = spec({
     // friendIsSuper: nextFriendSuper
 })
 
-const enqueueFriend = (state, move) => validMove(move)(snakeFromFriendSnake(state)) ? merge(state)({friendMoves: state.friendMoves.concat([move]), inverted: false}) : state;
+// const enqueueFriend = (state, move) => validMove(move)(snakeFromFriendSnake(state)) ? merge(state)({friendMoves: state.friendMoves.concat([move]), inverted: false}) : state;
 
 const enqueue = (state, move) => validMove(move)(state) ? merge(state)({moves: state.moves.concat([move])}) : state;
 
-module.exports = {EAST, NORTH, SOUTH, WEST, initialState, enqueue, next, enqueueFriend}
+module.exports = {EAST, NORTH, SOUTH, WEST, initialState, enqueue, next}
